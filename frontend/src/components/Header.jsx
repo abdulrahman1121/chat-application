cc587e5ef54eae9b7c3bdd062e5c319befe4f30a
abdulrahman1121 <abdallrahmaelmi456@gmail.com>
2024-08-14 12:38:28 -0700

    Change title of project and color of the names in the message body page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <div id='header'> 
             <div className="header-container">
-                <img src='/codedaylogo.png' alt='' className='logo'/>
+                <img src='/codedaylogo.png' alt='codeday-logo' className='logo'/>
                 <h1>Messages</h1>
                 <AddCircleRoundedIcon style={{fontSize: '60px'}} className='plus-icon'/>
             </div>
