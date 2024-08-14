fc539aca9529b4ae3ce57b52329de22ffe623f3c
Quentin Chen <quentinchen2164@example.com>
2024-08-13 21:09:56 -0700

    touching up header/adding message container/adding navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,13 @@
 import './Header.css'
-import AddCircleOutlinedIcon from '@mui/icons-material/AddCircleOutlined';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
 
 export default function Header() {
     return (
         <div id='header'> 
             <div className="header-container">
-                <FavoriteBorderOutlinedIcon style={{color: 'red', fontSize: '40px'}} className='heart-icon'/>
+                <img src='/codedaylogo.png' alt='' className='logo'/>
                 <h1>Messages</h1>
-                <AddCircleOutlinedIcon style={{color: 'green', fontSize: '80px'}} className='plus-icon'/>
-                <hr />
+                <AddCircleRoundedIcon style={{fontSize: '60px'}} className='plus-icon'/>
             </div>
         </div>
     )
