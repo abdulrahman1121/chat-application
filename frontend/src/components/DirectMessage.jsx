cbc8109ec4f7e7f9add5b9301752127de1da5c73
Quentin Chen <quentinchen2164@example.com>
2024-08-15 17:55:13 -0700

    reworked messaging page

---
+++ frontend/src/components/DirectMessage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DirectMessage.css';
+
+export default function DirectMessage() {
+    return (
+        <div className='direct-message-container'>
+            <div className='direct-message-header'>Sharanya</div>
+            <div className='direct-message-content'>Content goes here</div>
+            <div className='direct-message-messaging'>Write message here</div>
+        </div>
+    )
+}
