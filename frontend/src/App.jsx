fc8ba9af1e27b5235223f65cc6616a7ad20ea40c
Quentin Chen <quentinchen2164@example.com>
2024-08-29 00:16:24 -0700

    page-routing/navbar-functionality/message-page-ui-changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import MessagingPage from './pages/MessagingPage';
+import LandingPage from './pages/LandingPage'
+import AssignmentPage from './pages/AssignmentPage'
+import NoPage from './pages/NoPage'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 export default function App() {
     return (
-        <div id='app'>
-            <MessagingPage /> 
-        </div>
+        <Router>
+            <div id='app'>
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/home" element={<LandingPage />} />
+                    <Route path="/messages" element={<MessagingPage />} />
+                    <Route path="/assignments" element={<AssignmentPage />} />
+                    <Route path="*" element={<NoPage />} />
+                </Routes>
+            </div>
+        </Router>
     )
 }
